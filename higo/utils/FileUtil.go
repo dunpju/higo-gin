8c75155ec1740fe80521894f057d7ad861cbe784
dengpengju <dengpju@163.com>
2020-10-08 14:26:47 +0800

    利用反射获取文件相关时间信息避免跨平台问题

--- higo/utils/FileUtil.go
+++ higo/utils/FileUtil.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
+	"fmt"
 	"os"
-	"syscall"
+	"reflect"
 )
 
 // 自定义文件结构体
@@ -18,93 +19,83 @@ func NewFile(name string) *File {
 // 获取文件创建时间戳
 func (this *File) GetCreateTimestamp() int64 {
 	fileInfo, _ := os.Stat(this.Name)
-
-	stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-	return stat_t.CreationTime.Nanoseconds()/1e9
-
-	//sysType := runtime.GOOS
-	//if sysType == "windows" {
-	//	stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-	//	return stat_t.CreationTime.Nanoseconds()/1e9
-	//}
-	//
-	//// Sys()返回的是interface{}，所以需要类型断言，不同平台需要的类型不一样，linux上为*syscall.Stat_t;
-	//// windows 上为 *syscall.Win32FileAttributeData
-	//stat_t := fileInfo.Sys().(*syscall.Stat_t)
-	//return stat_t.Ctim.Sec
+	t := reflect.ValueOf(fileInfo.Sys())
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("CreationTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("CreationTime").Field(1).Uint()
+		return Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	}
+	return int64(t.Elem().FieldByName("Ctim").FieldByName("Sec").Int())
 }
 
 // 获取文件创建时间
-//func (this *File) GetCreateTime() string {
-//	fileInfo, _ := os.Stat(this.Name)
-//
-//	sysType := runtime.GOOS
-//	if sysType == "windows" {
-//		stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-//		return TimestampToTime(stat_t.CreationTime.Nanoseconds()/1e9)
-//	}
-//
-//	// Sys()返回的是interface{}，所以需要类型断言，不同平台需要的类型不一样，linux上为*syscall.Stat_t;
-//	// windows 上为 *syscall.Win32FileAttributeData
-//	stat_t := fileInfo.Sys().(*syscall.Stat_t)
-//	return TimestampToTime(stat_t.Ctim.Sec)
-//}
+func (this *File) GetCreateTime() string {
+	fileInfo, _ := os.Stat(this.Name)
+	t := reflect.ValueOf(fileInfo.Sys())
+	var sec int64
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("CreationTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("CreationTime").Field(1).Uint()
+		sec = Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	}else {
+		sec = int64(t.Elem().FieldByName("Ctim").FieldByName("Sec").Int())
+	}
+	return TimestampToTime(sec)
+}
 
 // 获取文件更新时间戳
-//func (this *File) GetModifyTimestamp() int64 {
-//	fileInfo, _ := os.Stat(this.Name)
-//
-//	sysType := runtime.GOOS
-//	if sysType == "windows" {
-//		stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-//		return stat_t.LastWriteTime.Nanoseconds()/1e9
-//	}
-//
-//	stat_t := fileInfo.Sys().(*syscall.Stat_t)
-//	return stat_t.Mtim.Sec
-//}
+func (this *File) GetModifyTimestamp() int64 {
+	fileInfo, _ := os.Stat(this.Name)
+	t := reflect.ValueOf(fileInfo.Sys())
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("LastWriteTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("LastWriteTime").Field(1).Uint()
+		return Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	}
+	return int64(t.Elem().FieldByName("Mtim").FieldByName("Sec").Int())
+}
 
 // 获取文件更新时间
-//func (this *File) GetModifyTime() string {
-//	fileInfo, _ := os.Stat(this.Name)
-//
-//	sysType := runtime.GOOS
-//	if sysType == "windows" {
-//		stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-//		return TimestampToTime(stat_t.LastWriteTime.Nanoseconds()/1e9)
-//	}
-//
-//	stat_t := fileInfo.Sys().(*syscall.Stat_t)
-//	return TimestampToTime(stat_t.Mtim.Sec)
-//}
+func (this *File) GetModifyTime() string {
+	fileInfo, _ := os.Stat(this.Name)
+	t := reflect.ValueOf(fileInfo.Sys())
+	var sec int64
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("LastWriteTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("LastWriteTime").Field(1).Uint()
+		sec = Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	} else {
+		sec = int64(t.Elem().FieldByName("Mtim").FieldByName("Sec").Int())
+	}
+	return TimestampToTime(sec)
+}
 
 // 获取文件访问时间戳
-//func (this *File) GetAccessTimestamp() int64 {
-//	fileInfo, _ := os.Stat(this.Name)
-//
-//	sysType := runtime.GOOS
-//	if sysType == "windows" {
-//		stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-//		return stat_t.LastAccessTime.Nanoseconds()/1e9
-//	}
-//
-//	stat_t := fileInfo.Sys().(*syscall.Stat_t)
-//	return stat_t.Atim.Sec
-//}
+func (this *File) GetAccessTimestamp() int64 {
+	fileInfo, _ := os.Stat(this.Name)
+	t := reflect.ValueOf(fileInfo.Sys())
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("LastAccessTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("LastAccessTime").Field(1).Uint()
+		return Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	}
+	return int64(t.Elem().FieldByName("Atim").FieldByName("Sec").Int())
+}
 
 // 获取文件访问时间
-//func (this *File) GetAccessTime() string {
-//	fileInfo, _ := os.Stat(this.Name)
-//
-//	sysType := runtime.GOOS
-//	if sysType == "windows" {
-//		stat_t := fileInfo.Sys().(*syscall.Win32FileAttributeData)
-//		return TimestampToTime(stat_t.LastAccessTime.Nanoseconds()/1e9)
-//	}
-//
-//	stat_t := fileInfo.Sys().(*syscall.Stat_t)
-//	return TimestampToTime(stat_t.Atim.Sec)
-//}
+func (this *File) GetAccessTime() string {
+	fileInfo, _ := os.Stat(this.Name)
+	t := reflect.ValueOf(fileInfo.Sys())
+	var sec int64
+	if "syscall.Win32FileAttributeData" == fmt.Sprintf("%s", t.Elem().Type()) {
+		lowDateTime := t.Elem().FieldByName("LastAccessTime").Field(0).Uint()
+		highDateTime := t.Elem().FieldByName("LastAccessTime").Field(1).Uint()
+		sec = Nanoseconds(uint32(lowDateTime), uint32(highDateTime)) / 1e9
+	}else {
+		sec = int64(t.Elem().FieldByName("Atim").FieldByName("Sec").Int())
+	}
+	return TimestampToTime(sec)
+}
 
 // 文件是否存在
 func (this *File) IsExist() bool {
--- higo/utils/TimeUtil.go
+++ higo/utils/TimeUtil.go
@@ -12,3 +12,14 @@ func CurrentTimestamp () int64  {
 func TimestampToTime(ts int64) string {
 	return time.Unix(int64(ts), 0).Format("2006-01-02 15:04:05")
 }
+
+// 纳秒
+func Nanoseconds(LowDateTime uint32, HighDateTime uint32) int64 {
+	// 100-nanosecond intervals since January 1, 1601
+	nsec := int64(HighDateTime)<<32 + int64(LowDateTime)
+	// change starting time to the Epoch (00:00:00 UTC, January 1, 1970)
+	nsec -= 116444736000000000
+	// convert into nanoseconds
+	nsec *= 100
+	return nsec
+}
