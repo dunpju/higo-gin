ca7f9296f0c129704a4414c4acde02484263e681
dengpengju <dengpju@163.com>
2020-12-08 18:12:25 +0800

    更新

--- test/bin/main.go
+++ test/bin/main.go
@@ -8,13 +8,18 @@ import (
 	"github.com/dengpju/higo-gin/test/app/Middlewares"
 	"github.com/dengpju/higo-gin/test/router"
 	"github.com/dengpju/higo-ioc/injector"
+	"github.com/dengpju/higo-utils/utils"
+	"go/ast"
 	"go/importer"
+	"go/parser"
+	"go/token"
 	"os/exec"
 )
 
 func main()  {
-
-	pkg, err := importer.Default().Import("exec")
+	scanFiles := utils.Dir("./test/app/Controllers").Suffix("go").Scan().List()
+	fmt.Println(scanFiles)
+	pkg, err := importer.Default().Import("os/exec")
 	if err != nil {
 		fmt.Println("error:", err)
 		return
@@ -22,6 +27,33 @@ func main()  {
 	for _, declName := range pkg.Scope().Names() {
 		fmt.Println(declName)
 	}
+
+	// 通过解析src来创建AST。
+	fset := token.NewFileSet() // 职位相对于fset
+	f, err := parser.ParseFile(fset, "./test/app/Controllers/V3/DemoController.go", nil, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		var s string
+		switch x := n.(type) {
+		case *ast.BasicLit:
+			s = x.Value
+		case *ast.Ident:
+			s = x.Name
+		case *ast.StructType:
+			s = "struct"
+			fmt.Println(x.Fields.)
+		}
+		if s != "" {
+			fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
+		}
+		return true
+	})
+
+	// 打印AST。
+	//_ = ast.Print(fset, f)
 
 	return
 
