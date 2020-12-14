9e0cc18b2b3afb51716ad021a38f891d6ef562f7
dengpengju <dengpju@163.com>
2020-12-14 19:03:40 +0800

    更新

--- higo/Dependency.go
+++ higo/Dependency.go
@@ -1,1 +1,21 @@
 package higo
+
+var container Dependency
+
+type Dependency map[string]IClass
+
+func init() {
+	container = make(Dependency)
+}
+
+// 注册到Di容器
+func AddDiToContainer(class IClass)  {
+	rt, _ := class.Reflection()
+	typ := rt.Name()
+	container[typ] = class
+}
+
+// 获取依赖
+func Di(name string) IClass {
+	return container[name]
+}
