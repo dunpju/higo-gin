774a8f0f07bab7fd80a49a81fe4803bf57699145
dengpengju <dengpju@163.com>
2021-06-27 16:51:22 +0800

    更新

--- higo/responser/Responser.go
+++ higo/responser/Responser.go
@@ -6,3 +6,8 @@ func SuccessJson(message string, code int, data interface{}) {
 	ctx := higo.Request.Context()
 	higo.Responser(ctx).SuccessJson(message, code, data)
 }
+
+func ErrorJson(message string, code int, data interface{}) {
+	ctx := higo.Request.Context()
+	higo.Responser(ctx).ErrorJson(message, code, data)
+}
