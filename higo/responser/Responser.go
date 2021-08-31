dd3f021622a3b69c0ca686131be1a7da5a090590
dengpengju <dengpju@163.com>
2021-08-31 21:55:17 +0800

    更新

--- higo/responser/Responser.go
+++ higo/responser/Responser.go
@@ -2,12 +2,12 @@ package responser
 
 import "github.com/dengpju/higo-gin/higo"
 
-func SuccessJson(message, code interface{}, data interface{}) {
+func SuccessJson(message string, code int, data interface{}) {
 	ctx := higo.Request.Context()
-	higo.Responser(ctx).SuccessJson(message.(string), code.(int), data)
+	higo.Responser(ctx).SuccessJson(message, code, data)
 }
 
-func ErrorJson(message, code interface{}, data interface{}) {
+func ErrorJson(message string, code int, data interface{}) {
 	ctx := higo.Request.Context()
-	higo.Responser(ctx).ErrorJson(message.(string), code.(int), data)
+	higo.Responser(ctx).ErrorJson(message, code, data)
 }
