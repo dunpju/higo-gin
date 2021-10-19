e06fe25b3a234f5103f93499b64eab6740ad327a
dengpengju <dengpju@163.com>
2021-10-19 21:52:57 +0800

    isnull

--- higo/sql/operate.go
+++ higo/sql/operate.go
@@ -5,38 +5,58 @@ import (
 	"strings"
 )
 
-func WhereIn(column string, values []interface{}) string {
+func WhereIn(column string, values interface{}) string {
 	conValues := make([]string, 0)
-	for _, value := range values {
-		if v, ok := value.(int); ok {
+	if value, ok := values.([]int); ok {
+		for _, v := range value {
 			conValues = append(conValues, utils.IntString(v))
-		} else if v, ok := value.(int64); ok {
-			conValues = append(conValues, utils.Int64String(v))
-		} else if v, ok := value.(float32); ok {
-			conValues = append(conValues, utils.FloatString(v))
-		} else if v, ok := value.(float64); ok {
-			conValues = append(conValues, utils.Float64String(v))
-		} else {
-			conValues = append(conValues, value.(string))
 		}
+	} else if value, ok := values.([]int64); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.Int64String(v))
+		}
+	} else if value, ok := values.([]float32); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.FloatString(v))
+		}
+	} else if value, ok := values.([]float64); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.Float64String(v))
+		}
+	} else if value, ok := values.([]string); ok {
+		conValues = value
+	} else {
+		panic("Unsupported types")
 	}
 	return column + " IN(" + strings.Join(conValues, ",") + ")"
 }
 
-func WhereNotIn(column string, values []interface{}) string {
+func WhereNotIn(column string, values interface{}) string {
 	conValues := make([]string, 0)
-	for _, value := range values {
-		if v, ok := value.(int); ok {
+	if value, ok := values.([]int); ok {
+		for _, v := range value {
 			conValues = append(conValues, utils.IntString(v))
-		} else if v, ok := value.(int64); ok {
-			conValues = append(conValues, utils.Int64String(v))
-		} else if v, ok := value.(float32); ok {
-			conValues = append(conValues, utils.FloatString(v))
-		} else if v, ok := value.(float64); ok {
-			conValues = append(conValues, utils.Float64String(v))
-		} else {
-			conValues = append(conValues, value.(string))
 		}
+	} else if value, ok := values.([]int64); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.Int64String(v))
+		}
+	} else if value, ok := values.([]float32); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.FloatString(v))
+		}
+	} else if value, ok := values.([]float64); ok {
+		for _, v := range value {
+			conValues = append(conValues, utils.Float64String(v))
+		}
+	} else if value, ok := values.([]string); ok {
+		conValues = value
+	} else {
+		panic("Unsupported types")
 	}
 	return column + " NOT IN(" + strings.Join(conValues, ",") + ")"
+}
+
+func IsNull(column string) string {
+	return "isnull(`" + column + "`)"
 }
