afc0bddbf6ceb8b3cf4b534f545e4d6e4720dd93
dengpju <dengpju@163.com>
2021-10-20 11:46:46 +0800

    更新

--- higo/sql/operate.go
+++ higo/sql/operate.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func WhereIn(column string, values interface{}) string {
+func convert(values interface{}) []string {
 	conValues := make([]string, 0)
 	if value, ok := values.([]int); ok {
 		for _, v := range value {
@@ -28,35 +28,21 @@ func WhereIn(column string, values interface{}) string {
 	} else {
 		panic("Unsupported types")
 	}
-	return column + " IN(" + strings.Join(conValues, ",") + ")"
+	return conValues
 }
 
-func WhereNotIn(column string, values interface{}) string {
-	conValues := make([]string, 0)
-	if value, ok := values.([]int); ok {
-		for _, v := range value {
-			conValues = append(conValues, utils.IntString(v))
-		}
-	} else if value, ok := values.([]int64); ok {
-		for _, v := range value {
-			conValues = append(conValues, utils.Int64String(v))
-		}
-	} else if value, ok := values.([]float32); ok {
-		for _, v := range value {
-			conValues = append(conValues, utils.FloatString(v))
-		}
-	} else if value, ok := values.([]float64); ok {
-		for _, v := range value {
-			conValues = append(conValues, utils.Float64String(v))
-		}
-	} else if value, ok := values.([]string); ok {
-		conValues = value
-	} else {
-		panic("Unsupported types")
-	}
-	return column + " NOT IN(" + strings.Join(conValues, ",") + ")"
+func IN(column string, values interface{}) string {
+	return column + " IN(" + strings.Join(convert(values), ",") + ")"
+}
+
+func NotIn(column string, values interface{}) string {
+	return column + " NOT IN(" + strings.Join(convert(values), ",") + ")"
 }
 
 func IsNull(column string) string {
 	return "isnull(`" + column + "`)"
+}
+
+func IF(expr1, expr2, expr3 string) string {
+	return "IF(" + expr1 + "," + expr2 + "," + expr3 + ")"
 }
