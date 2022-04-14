1b17b67e761c34caf8d600ac431b609e69b85ec1
dengpengju <dengpju@163.com>
2022-04-14 21:43:02 +0800

    更新

--- higo/templates/Code.go
+++ higo/templates/Code.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dengpju/higo-utils/utils"
 	"github.com/dengpju/higo-utils/utils/dirutil"
 	"github.com/dengpju/higo-utils/utils/fileutil"
+	"github.com/dengpju/higo-utils/utils/maputil"
 	"github.com/dengpju/higo-utils/utils/stringutil"
 	"io/ioutil"
 	"log"
@@ -40,9 +41,134 @@ type Code struct {
 	Codes     []*Code
 }
 
+type CodeBuilder struct {
+	Name         string
+	Len          int
+	FuncName     string
+	KeyValueDocs []maputil.KeyValueDoc
+	OutDir       string
+}
+
+func NewCodeBuilder(name string) *CodeBuilder {
+	return &CodeBuilder{Name: name}
+}
+
+func (this *CodeBuilder) parse(doc string) *CodeBuilder {
+	messagePattern := `(@Message\(").*?("\))`
+	messageRegexp := regexp.MustCompile(messagePattern)
+	if messageRegexp == nil {
+		log.Fatalln("regexp err")
+	}
+	messages := messageRegexp.FindAllStringSubmatch(doc, -1)
+	message := make([]string, 0)
+	for _, msg := range messages {
+		msg0 := strings.Replace(msg[0], "\n", "", -1)
+		msg0 = strings.Replace(msg0, `@Message("`, "", -1)
+		msg0 = strings.Replace(msg0, `")`, "", -1)
+		message = append(message, msg0)
+	}
+
+	codePattern := `(const\s+).*?(;)`
+	codeRegexp := regexp.MustCompile(codePattern)
+	if codeRegexp == nil {
+		log.Fatalln("regexp err")
+	}
+	codes := codeRegexp.FindAllStringSubmatch(doc, -1)
+	codd := make([]maputil.KeyValueDoc, 0)
+	for i, cod := range codes {
+		cod0 := strings.Replace(cod[0], "\n", "", -1)
+		cod0 = strings.Replace(cod0, `const `, "", -1)
+		cod0 = strings.Replace(cod0, `;`, "", -1)
+		codSplit := strings.Split(cod0, "=")
+		key := utils.String.CaseToCamel(strings.ToLower(strings.Trim(codSplit[0], "")))
+		value := strings.ToLower(strings.Trim(codSplit[1], ""))
+		codd = append(codd, *utils.Map.NewKeyValueDoc(key, value, message[i]))
+	}
+	this.KeyValueDocs = codd
+	return this
+}
+
+func (this *CodeBuilder) generate() {
+	keyValueDocs := this.KeyValueDocs
+	this.Len = len(this.KeyValueDocs) - 1
+	if len(keyValueDocs) > 0 {
+		utils.Dir.Open(this.OutDir).Create()
+		File := this.OutDir + utils.Dir.Separator() + strings.Replace(keyValueDocs[0].Value.(string), " ", "", -1) + ".go"
+		this.FuncName = "code" + strings.Replace(keyValueDocs[0].Value.(string), " ", "", -1)
+		if utils.File.Exist(File) {
+			log.Println(File + " already existed")
+			return
+		}
+		outFile := utils.File.New(File, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0755)
+		defer outFile.Close()
+		tpl := this.Template("code.tpl")
+		tmpl, err := template.New("code.tpl").Parse(tpl)
+		if err != nil {
+			panic(err)
+		}
+		//生成
+		err = tmpl.Execute(outFile.File(), this)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("code file " + File + " generate success!")
+	}
+}
+
+func (this *CodeBuilder) Template(tplfile string) string {
+	return tpl(tplfile)
+}
+
+func tpl(tplfile string) string {
+	_, file, _, _ := runtime.Caller(0)
+	file = path.Dir(file) + utils.Dir.Separator() + tplfile
+	f, err := os.Open(file)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	context, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	return string(context)
+}
+
+type Autoload struct {
+	FuncNames []string
+	OutDir    string
+}
+
+func NewAutoload(funcNames []string) *Autoload {
+	return &Autoload{FuncNames: funcNames}
+}
+
+func (this *Autoload) generate() {
+	utils.Dir.Open(this.OutDir).Create()
+	File := this.OutDir + utils.Dir.Separator() + "Autoload.go"
+	outFile := utils.File.New(File, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0755)
+	defer outFile.Close()
+	tpl := this.Template("autoload.tpl")
+	tmpl, err := template.New("autoload.tpl").Parse(tpl)
+	if err != nil {
+		panic(err)
+	}
+	//生成
+	err = tmpl.Execute(outFile.File(), this)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Autoload file " + File + " generate success!")
+}
+
+func (this *Autoload) Template(tplfile string) string {
+	return tpl(tplfile)
+}
+
 var (
 	codeRegexpStr = `(-c=[a-zA-Z_]+\s*-i=[0-9]+\s*-f=).*`
 	autoRegexpStr = `(\s*-auto).*`
+	funcNames     []string
 )
 
 // go run test\bin\main.go -gen=code -name="test\bin\md -auto" -out=test\app\Codes
@@ -62,14 +188,21 @@ func NewCode(pkg string, name string, file string) *Code {
 		if auto := reg.ReplaceAllString(name, ""); auto != "" {
 			name = auto
 		}
-		outfile := utils.File.ReadFile(name)
+		outfile := utils.File.Read(name)
 		if !outfile.Exist() {
 			log.Fatalln(name + " configure file non-exist")
 		}
 		if outfile.IsDir() {
 			files := utils.Dir.Open(name).Suffix("md").Scan().Get()
 			for _, filePath := range files {
-
+				fileContext := string(utils.File.Read(filePath).ReadAll())
+				build := NewCodeBuilder("CodeErrorCode").parse(fileContext)
+				build.generate()
+				funcNames = append(funcNames, build.FuncName)
+			}
+			log.Fatalln("ddd")
+			if len(funcNames) > 0 {
+				NewAutoload(funcNames).generate()
 			}
 		} else {
 			err := outfile.ForEach(func(line int, b []byte) {
