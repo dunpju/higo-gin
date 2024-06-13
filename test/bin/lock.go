63f9db97bedd3e07486f9ff0c83da51435a3dab5
dengpju <dengpju@163.com>
2024-06-13 21:51:49 +0800

    更新

--- test/bin/lock.go
+++ test/bin/lock.go
@@ -15,47 +15,58 @@ func main() {
 	fmt.Println(fmt.Sprintf("%p", v1), ok1)
 
 	wait := sync.WaitGroup{}
-	wait.Add(1)
-	go func() {
-		defer wait.Done()
-		//r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		//num := r.Intn(128)
-		//time.Sleep(time.Duration(num))
-		higo.Lock("test", func() {
-			fmt.Println("开始执行锁3")
-			time.Sleep(1)
-			fmt.Println("执行完成3")
-		})
-	}()
-	wait.Add(1)
-	go func() {
-		defer wait.Done()
-		//r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		//num := r.Intn(128)
-		//time.Sleep(time.Duration(num))
-		//higo.Retry(100*time.Millisecond, 3, "test", func() {
-		//	fmt.Println("开始执行锁2")
-		//	time.Sleep(1)
-		//	fmt.Println("执行完成2")
-		//})
-		higo.Lock("test", func() {
-			fmt.Println("开始执行锁2")
-			time.Sleep(1)
-			fmt.Println("执行完成2")
-		})
-	}()
-	wait.Add(1)
-	go func() {
-		defer wait.Done()
-		//r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		//num := r.Intn(128)
-		//time.Sleep(time.Duration(num))
-		higo.Lock("test", func() {
-			fmt.Println("开始执行锁1")
-			time.Sleep(10)
-			fmt.Println("执行完成1")
-		})
-	}()
+	for i := 10; i > 0; i-- {
+		wait.Add(1)
+		go func(i int) {
+			defer wait.Done()
+			//r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			//num := r.Intn(128)
+			//time.Sleep(time.Duration(num))
+			if i == 5 {
+				lockOk := higo.Retry(&higo.Returner{Interval: 100 * time.Microsecond, Retry: 3}, "test", func() {
+					fmt.Println(fmt.Sprintf("%d开始执行锁", i))
+					fmt.Println(fmt.Sprintf("%d执行完成", i))
+				})
+				fmt.Printf("lockOk: %v %d\n", lockOk, i)
+			} else {
+				lockOk := higo.Lock("test", func() {
+					fmt.Println(fmt.Sprintf("%d开始执行锁", i))
+					fmt.Println(fmt.Sprintf("%d执行完成", i))
+				})
+				fmt.Printf("lockOk: %v %d\n", lockOk, i)
+			}
+		}(i)
+	}
+	/*
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			//r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			//num := r.Intn(128)
+			//time.Sleep(time.Duration(num))
+			//higo.Retry(100*time.Millisecond, 3, "test", func() {
+			//	fmt.Println("开始执行锁2")
+			//	time.Sleep(1)
+			//	fmt.Println("执行完成2")
+			//})
+			higo.Lock("test", func() {
+				fmt.Println("开始执行锁2")
+				time.Sleep(1)
+				fmt.Println("执行完成2")
+			})
+		}()
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			//r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			//num := r.Intn(128)
+			//time.Sleep(time.Duration(num))
+			higo.Lock("test", func() {
+				fmt.Println("开始执行锁1")
+				time.Sleep(10)
+				fmt.Println("执行完成1")
+			})
+		}()*/
 	wait.Wait()
 
 }
